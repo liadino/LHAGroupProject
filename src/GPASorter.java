84dd3833f8cfe2b589d899b21c9df9707d312300
liadino <60626130+liadino@users.noreply.github.com>
2021-01-30 14:04:21 -0700

    Second Lia Commit

---
+++ src/GPASorter.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class GPASorter implements Comparator<student>
+			 	{
+	public int compare(student s1, student s2) {
+		return s1.getGPA().compareTo(s2.getLastName());
+	}
+
+
+}
